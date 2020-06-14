80e9df419c343b08467f5942021ac37e9b2ad0eb
soumya <example@example.com>
2020-06-14 19:30:22 +0530

    Random Advisor

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+function App() {
+  const [advice, updateAdvice] = useState("");
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(data => {
+        updateAdvice(data.slip.advice);
+      });
+  };
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  return (
+    <div className="app">
+      <div className="card col-10 col-md-6 d-flex justify-content-center">
+        <h2 className="heading">{advice}</h2>
+        <button className="button learn-more" onClick={() => fetchAdvice()}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+export default App;
